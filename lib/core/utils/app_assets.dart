b8c5c00e9cd4e71860a63bce08b556f635ad7e0a
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-07-23 16:12:37 +0300

    Added shared preferences and connctivity plugins

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,7 @@
+abstract class AppImages {
+  static const String _baseUrl = 'assets/images/';
+}
+
+abstract class AppSvgs {
+  static const String _baseUrl = 'assets/svgs/';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app_new_design/core/cache/shared_preferences.dart';
 import 'package:movie_app_new_design/core/di/di.dart';
 import 'package:movie_app_new_design/firebase_options.dart';
 
@@ -13,6 +14,7 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtil.ensureScreenSize();
+  await SharedPreferencesHelper.init();
 
   runApp(const MovieApp());
 }
