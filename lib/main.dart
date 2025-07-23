bca3fe5a102d282fe406cc417c258431b89e3b04
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-07-23 16:06:17 +0300

    Apply firebase on app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app_new_design/core/di/di.dart';
+import 'package:movie_app_new_design/firebase_options.dart';
 
 import 'movie_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   configureDependencies();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await ScreenUtil.ensureScreenSize();
 
   runApp(const MovieApp());
