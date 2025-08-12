9abaf32c3a4552cd1e4b013ea4c3ab0430384de4
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-08-12 22:48:13 +0300

    Created home screen cubit & make initial route of app dynamic

---
+++ lib/features/home/cubit/home_screen_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+import 'home_screen_states.dart';
+
+@injectable
+class HomeScreenViewModel extends Cubit<HomeScreenState> {
+  HomeScreenViewModel() : super(const HomeScreenInitial());
+
+  int currentIndex = 0;
+
+  void changeTab(int index) {
+    currentIndex = index;
+    emit(ChangeTabState(currentIndex));
+  }
+}
