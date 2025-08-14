e7d7c671703aaabeaac68cf8bf3aa56729a16f4d
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-08-14 04:00:17 +0300

    Improve the performance of functions that fetch genres.

--- lib/core/utils/get_movies_genres.dart
+++ lib/core/utils/get_movies_genres.dart
@@ -5,53 +5,65 @@ import 'package:dio/dio.dart';
 
 const String baseUrl = "https://yts.mx/api/v2/list_movies.json";
 const int limit = 50;
+const int maxPagesPerBatch = 20;
 
 final Dio dio = Dio();
 
-Future<Set<String>> getGenres() async {
-  final Set<String> genresSet = <String>{};
+Future<Set<String>> fetchGenresBatch(int startPage, int endPage) async {
+  final Set<String> batchGenres = <String>{};
+
+  final futures = <Future<Response>>[];
+  for (int page = startPage; page <= endPage; page++) {
+    final String url = "$baseUrl?limit=$limit&page=$page";
+    futures.add(dio.get(url));
+  }
+
+  final List<Response<dynamic>> responses = await Future.wait(futures);
+
+  for (final response in responses) {
+    if (response.statusCode == 200) {
+      final data =
+          response.data is String ? jsonDecode(response.data) : response.data;
+      final movies = data["data"]["movies"];
+      if (movies != null) {
+        for (final movie in movies) {
+          final List<dynamic>? genres = movie["genres"] as List<dynamic>?;
+          if (genres != null) {
+            batchGenres.addAll(genres.map((genre) => genre.toString()));
+          }
+        }
+      }
+    }
+  }
+
+  return batchGenres;
+}
+
+Future<Set<String>> getAllGenres() async {
+  final Set<String> allGenres = <String>{};
   int page = 1;
 
   while (true) {
     log("Fetching page $page...");
-    final String url = "$baseUrl?limit=$limit&page=$page";
 
-    try {
-      final Response response = await dio.get(url);
+    final Set<String> batchGenres = await fetchGenresBatch(
+      page,
+      page + maxPagesPerBatch - 1,
+    );
 
-      if (response.statusCode != 200) {
-        log("Error fetching data");
-        break;
-      }
-
-      final data =
-          response.data is String ? jsonDecode(response.data) : response.data;
-
-      final movies = data["data"]["movies"];
-      if (movies == null || movies.isEmpty) {
-        break;
-      }
-
-      for (final movie in movies) {
-        final genres = movie["genres"] as List<dynamic>?;
-        if (genres != null) {
-          genresSet.addAll(genres.map((genre) => genre.toString()));
-        }
-      }
-
-      page++;
-      await Future.delayed(Duration(milliseconds: 200));
-    } catch (e) {
-      log("Request error: $e");
+    if (batchGenres.isEmpty) {
       break;
     }
+
+    allGenres.addAll(batchGenres);
+    page += maxPagesPerBatch;
   }
 
-  return genresSet;
+  return allGenres;
 }
 
 Future<void> main() async {
-  final List<String> genresList = (await getGenres()).toList()..sort();
+  final List<String> genresList = (await getAllGenres()).toList()..sort();
   final File file = File("genres.json");
   await file.writeAsString(JsonEncoder.withIndent('  ').convert(genresList));
   log("Saved ${genresList.length} genres to genres.json");
